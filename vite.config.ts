59a341ec91caa564f73271001fc2488adb3cc209
刘宇阳 <3311118881@qq.com>
2023-06-29 17:54:43 +0800

    完成Vue、Element自动按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+
 // 配置@路径别名
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   // ↓解析配置
   resolve: {
     // ↓路径别名
     alias: {
       "@": resolve(__dirname, "./src")
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;"`,
+      },
+    },
   },
 })
