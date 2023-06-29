680bb25d7a13c1113c95f7da6125efb8e18a1a37
刘宇阳 <3311118881@qq.com>
2023-06-29 21:10:21 +0800

    导航列表

--- src/views/Layout/components/Sidebar.vue
+++ src/views/Layout/components/Sidebar.vue
@@ -7,19 +7,66 @@
     <div class="logo">
       <a href="javascript:;">Thrive</a>
     </div>
+
+    <!-- 导航列表 -->
+    <div class="list">
+      <ul>
+        <li class="item">
+          <a href="javascript:;" class="nav">仪表盘</a>
+        </li>
+
+        <li class="item">
+          <a href="javascript:;" class="nav">创作</a>
+        </li>
+
+        <li class="item">
+          <a href="javascript:;" class="nav">网站管理</a>
+        </li>
+
+        <li class="item">
+          <a href="javascript:;" class="nav">系统配置</a>
+        </li>
+
+        <li class="item">
+          <a href="javascript:;" class="nav">主题设置</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .Sidebar {
   .logo {
-    padding-top: 10px;
+    padding: 20px 0;
     font-size: 25px;
     font-weight: 900;
     text-align: center;
 
     a {
       color: #fff;
+    }
+  }
+
+  .item {
+    line-height: 50px;
+    padding-left: 20px;
+
+    // 鼠标经过导航高亮
+    &:hover a {
+      color: #fff;
+    }
+
+    .nav {
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      color: #dadada;
+
+      // 鼠标点击后高亮
+      &:focus {
+        color: #fff;
+      }
     }
   }
 }
