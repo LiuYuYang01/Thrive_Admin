6f8f39f0978ae34d470bc6cff62ed9ada5a344c1
刘宇阳 <3311118881@qq.com>
2023-07-04 09:29:55 +0800

    引入pinia

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 引入图标库
 import 'boxicons'
 import 'boxicons/dist/boxicons'
 
+// 引入store
+import pinia from '@/stores/index'
+
+// 引入路由
 import router from './routers'
+
+// 引入全局样式
 import '@/styles/index.scss'
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
