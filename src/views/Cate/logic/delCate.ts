46c6f3c27cd977de8439a45e9947dee994112510
刘宇阳 <3311118881@qq.com>
2023-07-07 11:38:34 +0800

    优化删除分类写法

--- src/views/Cate/logic/delCate.ts
+++ src/views/Cate/logic/delCate.ts
@@ -6,17 +6,13 @@ export const delCate = async (id: number, level: any) => {
     level = level >= 1 || level === 0 ? 'one' : 'two'
 
     // 根据level决定删除一级还是二级分类
-    if (level === "one") {
-        var res: any = await delCateAPI(id, "one")
-    } else if (level === "two") {
-        var res: any = await delCateAPI(id, "two")
-    }
+    const { code, message } = await delCateAPI(id, level)
 
-    if (res.code !== 200) return
+    if (code !== 200) return
 
     ElNotification({
         title: '成功',
-        message: res?.message,
+        message: message,
         type: 'success',
     })
 
