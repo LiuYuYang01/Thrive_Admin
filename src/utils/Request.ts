81a2ca418c412e6ed7a9d3ef2e7ffb38b22d1d39
LiuYuYang01 <3311118881@qq.com>
2024-01-20 17:49:51 +0800

    最新提交

--- src/utils/Request.ts
+++ src/utils/Request.ts
@@ -5,8 +5,8 @@ import { ElNotification } from "element-plus";
 import { useUserStore } from "@/stores";
 
 // 配置项目API域名
-// export const baseURL = "http://localhost:5000/api";
-export const baseURL = "http://139.196.43.147:5000/api"
+export const baseURL = "http://localhost:5000/api";
+// export const baseURL = "http://139.196.43.147:5000/api"
 
 // 创建 axios 实例
 const instance = axios.create({
