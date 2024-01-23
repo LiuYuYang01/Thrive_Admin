7b5b72eadca5ede96c66cc69edb899affcd28f26
LiuYuYang01 <3311118881@qq.com>
2024-01-23 20:04:33 +0800

    完成删除标签页功能

--- src/views/Layout/components/TabPage.vue
+++ src/views/Layout/components/TabPage.vue
@@ -1,48 +1,61 @@
 <script setup lang="ts">
+import { CircleCloseFilled } from '@element-plus/icons-vue'
+
 const router = useRouter()
 
 // 当前选中的标签页
 const active = ref<number | undefined>(0)
 
 interface Item {
-    name: string;
+    id: number
+    name: string
     path: string
 }
 
-// const list = ref([
-//     { name: "仪表盘", path: "/home" },
-//     { name: "文章管理", path: "/manage/article" },
-//     { name: "分类管理", path: "/manage/cate" },
-//     { name: "标签管理", path: "/manage/tag" },
-//     { name: "轮播图管理", path: "/manage/swiper" },
-// ])
-
 const list = ref<Item[]>([
-    { name: "仪表盘", path: "/home" }
+    { id: 0, name: "仪表盘", path: "/home" }
 ])
+
+// 跳转标签页
+const go = (item: Item, e: any) => {
+    const domName: string = e.target!.nodeName
+    console.log(item.id, active.value, 333);
+    if (domName !== "LI") {
+        list.value = list.value.filter(k => k.id !== item.id)
+        console.log(list.value, 888);
+
+        return
+    }
+
+    active.value = item.id
+    router.push(item.path)
+    console.log(item.id, active.value, 444);
+}
 
 // 监听当前页面路由变化，根据当前路由选中对应的标签页
 watch(() => router.currentRoute.value, r => {
-    console.log(r);
-    const data: Item = { name: r.meta.title as string, path: r.path }
-
-    console.log(list.value.some(item => item.path === data.path),555);
-    
     // 如果标签页中已经有这个数据了，就不让他在添加了
-    if (!list.value.some(item => item.path === data.path)) {
+    if (!list.value.some(item => item.path === r.path)) {
+        const id = list.value[list.value.length - 1].id + 1 as number
+
+        const data: Item = { id, name: r.meta.title as string, path: r.path }
         list.value?.push(data)
     }
 
-    active.value = list.value?.findIndex(item => item.path === r.path)
+    // 点击侧边导航后与标签页对应
+    active.value = list.value.find(item => item.path === r.path)!.id as number
 }, { immediate: true })
 </script>
 
 <template>
     <div class="tabPage">
         <ul>
-            <li v-for="(item, index) in list" @click="$router.push(item.path); active = index"
-                :class="active === index ? 'item active' : 'item'">
-                {{ item.name }}</li>
+            <li v-for="item in list" @click="go(item, $event)" :class="active === item.id ? 'item active' : 'item'">
+                {{ item.name }}
+                <el-icon class="close">
+                    <CircleCloseFilled />
+                </el-icon>
+            </li>
         </ul>
     </div>
 </template>
@@ -58,8 +71,20 @@ watch(() => router.currentRoute.value, r => {
         position: relative;
         z-index: 1;
 
+        // 选中的标签页样式
+        .active {
+            color: $color;
+            background-color: rgb(59 130 246 / 0.07);
+
+            &::before {
+                background-color: $color !important;
+            }
+        }
+
         .item {
-            padding: 10px 20px;
+            position: relative;
+            padding: 10px;
+            padding-left: 20px;
             border-right: 1px solid #eee;
             background-color: $move;
             border-bottom: 1px solid #eee;
@@ -76,14 +101,43 @@ watch(() => router.currentRoute.value, r => {
                 background-color: #ddd;
                 transition: background-color $move;
             }
-        }
 
-        .active {
-            color: $color;
-            background: rgb(59 130 246 / 0.07);
+            .close {
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                width: 0;
+                color: #d6d6d6;
+                opacity: 0;
+                transition: all $move;
+                z-index: 3;
 
-            &::before {
-                background-color: $color;
+                &:hover {
+                    color: #FF1654;
+                }
+            }
+
+            // 鼠标经过显示删除按钮
+            &:hover {
+                padding-right: 35px;
+                color: #666;
+                background-color: rgb(59 130 246 / 0.07);
+
+                .close {
+                    width: 30px;
+                    height: 100%;
+                    opacity: 1;
+                }
+            }
+
+            // 第一个标签页不需要显示删除按钮
+            &:first-child {
+                padding-right: 10px !important;
+
+                .close {
+                    display: none;
+                    height: 40px;
+                }
             }
         }
     }
